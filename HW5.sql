c4515146c371a6cfe2136b4067e63fb5a35e0918
Burakalankus <118599126+Burakalankus@users.noreply.github.com>
2023-12-31 18:28:57 +0300

    Add files via upload

---
+++ HW5.sql
@@ -0,0 +1,18 @@
+-- Solution 1 
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+-- Solution 2
+SELECT * FROM film 
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+
+-- Solution 3
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
